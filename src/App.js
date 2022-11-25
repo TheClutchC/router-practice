e45d27787efab7fecad04044abecdaa5cab6b3c6
Clay Carney <claycarney7@gmail.com>
2022-11-25 11:26:18 -0500

    add missing imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
