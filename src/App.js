af6f73049bd681365e78ef9e22a945b69e478b44
Clay Carney <claycarney7@gmail.com>
2022-11-25 11:41:56 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
-    <>
-      <BrowserRouter basename='/router-practice'>
-        <Routes>
-          <Route exact path='/router-practice' element={<HomePage />} />
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      <Route exact path='/router-practice' element={<HomePage />} />
+      <Route path='/about' element={<AboutPage />} />
+    </div>
   );
 }
 
